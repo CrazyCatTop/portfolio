0d6837990ff4634171d581f77bede17943d78644
Cat <daniliukdaniil@gmail.com>
2023-01-03 01:37:19 +0300

    resume screen, correction of other screens & menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,8 @@
 const hamburger = document.querySelector('.hamburger'),
       menu = document.querySelector('.menu'),
-      closeCross = document.querySelector('.menu__close');
+      closeCross = document.querySelector('.menu__close'),
+      menu_links = document.querySelectorAll('.menu__link'), 
+      menu_social_links = document.querySelector('.menu__social');
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
@@ -8,4 +10,16 @@ hamburger.addEventListener('click', () => {
 
 closeCross.addEventListener('click', function() {
     menu.classList.remove('active');
+});
+
+menu_links.forEach(function(link) {
+    link.querySelector('a').addEventListener('click', function() {
+        menu.classList.remove('active');
+    });
+});
+
+menu_social_links.querySelectorAll('a').forEach(function(link) {
+    link.addEventListener('click', function() {
+        menu.classList.remove('active');
+    });
 });
